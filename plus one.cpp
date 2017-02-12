812da40b396711b4697bf841797e26b0d44c9f01
wuheng0723 <dongjiyao@stu.xjtu.edu.cn>
2017-02-12 20:48:42 +0800

    leetcode problem:plus one
    vector的前端为高位，注意进位

---
+++ plus one.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        int carry = 1;
+        for(int i=digits.size()-1;i>=0;i--)
+        {
+            if(carry == 1)
+                digits[i]++;
+            if(digits[i]>9)
+            {
+                digits[i] = digits[i]%10;
+                carry = 1;
+            }
+            else
+            {
+                carry = 0;
+                break;
+            }
+
+        }
+        if(carry == 1)
+            digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
